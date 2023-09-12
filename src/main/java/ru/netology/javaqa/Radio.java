351c0dae945b68d4025f89dfe0113f2aa9a9e4ca
Bolotjanovna <toksonbaeva2104@gmail.com>
2023-09-13 01:32:22 +0500

    Testing Radio Station and Volume

--- src/main/java/ru/netology/javaqa/Radio.java
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -24,7 +24,7 @@ public class Radio {
         this.currentStation = currentStation;
     }
 
-//    private int currentStation;
+   private int currentStation;
 
     public int getCurrentVolume() {
         return currentVolume;
